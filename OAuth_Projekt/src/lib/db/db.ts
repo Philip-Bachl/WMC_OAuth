98c13097198cf5315a1262152b8a6c6e99f993c7
Philip-Bachl <p.bachl@icloud.com>
2025-04-09 15:29:26 +0200

    type bugfix

--- OAuth_Projekt/src/lib/db/db.ts
+++ OAuth_Projekt/src/lib/db/db.ts
@@ -1,11 +1,12 @@
-import sqlite3, { Database } from "sqlite3";
+import sqlite3 from "sqlite3";
+import type { Database } from "sqlite3";
 import { schemaScript } from "./schema";
 
 const CONNECTION_STRING = "db.db";
 
 export function createDbClient(): Database {
     const db = new sqlite3.Database(CONNECTION_STRING, (err) => {
-        if(err) {
+        if (err) {
             //TODO
             throw err;
         }
